4f6c0b09bcd5e284cb80f024c7198d2cd3a5adcf
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-18 17:37:44 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,4 +16,6 @@ def help
 - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
-- exit : exits this program
+- exit : exits this program"
+
+end 
